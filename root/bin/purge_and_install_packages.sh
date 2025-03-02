1a429887e0faefabecbf2a2acfdb8ab680ba92a4
Ramon Fischer <Ramon_Fischer@hotmail.de>
2025-03-02 21:07:09 +0100

    download gpg key

---
+++ root/bin/purge_and_install_packages.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# purge packages and their configuration files
+/usr/bin/apt purge \
+    avahi-daemon \
+    nano \
+    vim-common
+
+# update package cache
+/usr/bin/apt update
+
+# install packages
+/usr/bin/apt install \
+    neovim
+
+# install "log2ram"; see also "/etc/apt/sources.list.d/azlux.list"
+/usr/bin/wget "https://azlux.fr/repo.gpg" \
+    --output-document="/usr/share/keyrings/azlux-archive-keyring.gpg"
+/usr/bin/apt install \
+    log2ram
